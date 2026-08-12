8f134df82e9cbe5f0c1dbecedfcf2a4ac3329b26
jvbgomes <jvbg16@gmail.com>
2026-08-12 19:13:04 -0300

    estudos: condicional simples com boolean e if/else

--- estudos/maratona-java/01-introducao/src/academy/devdojo/maratonajava/intro/Condicionais.java
+++ estudos/maratona-java/01-introducao/src/academy/devdojo/maratonajava/intro/Condicionais.java
@@ -3,17 +3,12 @@ package academy.devdojo.maratonajava.intro;
 public class Condicionais {
     public static void main(String[] args) {
         int idade = 20;
-        int idade2 = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
-        boolean isAutorizadoComprarBebida2 = idade2 >= 18;
         // !
         if (isAutorizadoComprarBebida) {
             System.out.println("Autorizado a comprar bebida alcolica");
-        }
-
-        if (isAutorizadoComprarBebida2 == false) {
+        } else {
             System.out.println("Nao esta autorizado a comprar bebida alcolica");
-
         }
     }
 }

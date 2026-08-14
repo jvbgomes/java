3f6719999cab3694536e95664359c56408bded99
jvbgomes <jvbg16@gmail.com>
2026-08-13 23:33:49 -0300

    estudos: reorganize modulos e finaliza secao de arrays
    Move arquivos do pacote intro para seus modulos corretos
    (introducao, operadores, condicionais, repeticoes, arrays),
    atualizando a declaracao de package em cada classe.

---
+++ estudos/maratona-java/05-arrays/src/academy/devdojo/maratonajava/arrays/Arrays2.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.arrays;
+
+public class Arrays2 {
+    static void main(String[] args) {
+        int[] idades = new int[4];
+        idades[0] = 21;
+        idades[1] = 14;
+        idades[2] = 11;
+        idades[3] = 53;
+
+        System.out.println(idades[0]);
+    }
+}

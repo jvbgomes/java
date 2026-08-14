30f5bff3e742dd304b19385cf17007baf0b1d182
jvbgomes <jvbg16@gmail.com>
2026-08-13 22:30:20 -0300

    Arrays em java

--- estudos/maratona-java/01-introducao/src/academy/devdojo/maratonajava/intro/Arrays.java
+++ estudos/maratona-java/01-introducao/src/academy/devdojo/maratonajava/intro/Arrays.java
@@ -2,6 +2,32 @@ package academy.devdojo.maratonajava.intro;
 
 public class Arrays {
     public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
 
+        String[] persons = new String[8];
+
+        persons[0] = "João";
+        persons[1] = "Gabe";
+        persons[2] = "Mk";
+        persons[3] = "Radla";
+        persons[4] = "Felipe";
+        persons[5] = "Arthur";
+        persons[6] = "Nicollas";
+        persons[7] = "Vinicius";
+
+        //there is other way of doing a for loop in java, which follows bellow
+
+        for (String person : persons) {
+            System.out.println(person);
+        }
+
+        System.out.println("---------------------");
+
+        for (int i = 0; i < persons.length; i++) {
+            System.out.println(persons[i]);
+        }
     }
 }
+
+

d82c66db5330b39fcc8a234113c89ae922dc0989
jvbgomes <jvbg16@gmail.com>
2026-08-19 03:49:56 -0300

    CalculadoraTest04: testa passagem por valor de primitivos

--- estudos/maratona-java/06-orientacao-objeto/src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest04.java
+++ estudos/maratona-java/06-orientacao-objeto/src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest04.java
@@ -1,7 +1,16 @@
 package academy.devdojo.maratonajava.javacore.introducaometodos.test;
 
+import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Calculadora;
+
 public class CalculadoraTest04 {
     static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int numero1 = 1;
+        int numero2 = 2;
+        calculadora.alteraDoisNumeros(numero1, numero2);
+        System.out.println("Dentro CalculadoraTest04");
+        System.out.println(numero1);
+        System.out.println(numero2);
 
     }
 }

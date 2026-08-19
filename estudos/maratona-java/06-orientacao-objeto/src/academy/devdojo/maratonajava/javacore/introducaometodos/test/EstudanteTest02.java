0ef08d43223fc51d97a0c485156eacd9230f4c6c
jvbgomes <jvbg16@gmail.com>
2026-08-19 03:54:17 -0300

    EstudanteTest02: adiciona arquivo de teste para Estudante

---
+++ estudos/maratona-java/06-orientacao-objeto/src/academy/devdojo/maratonajava/javacore/introducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Estudante;
+
+public class EstudanteTest02 {
+    static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+    }
+}

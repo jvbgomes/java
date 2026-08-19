082a5bda213fd0dc9694ad2f93707569c16a0a41
jvbgomes <jvbg16@gmail.com>
2026-08-19 03:54:30 -0300

    Pessoa: dominio com encapsulamento e validacao de idade

---
+++ estudos/maratona-java/06-orientacao-objeto/src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Pessoa.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
+
+public class Pessoa {
+    private String name;
+    private int age;
+
+    public void print() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Invalid age");
+            return;
+        }
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+}

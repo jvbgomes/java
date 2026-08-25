3adca49b423ae5634be99da2ec53c9c0b7644e63
jvbgomes <jvbg16@gmail.com>
2026-08-24 23:55:59 -0300

    Rename LivroIndisponivelException to BookNotAvailableException

--- estudos/maratona-java/06-orientacao-objeto/src/treino/pratica/orientacaoobjetos/praticando/domain/LivroIndisponivelException.java
+++
@@ -1,7 +0,0 @@
-package treino.pratica.orientacaoobjetos.praticando.domain;
-
-public class LivroIndisponivelException extends RuntimeException {
-    public LivroIndisponivelException(String livroJaEstaEmprestado) {
-        super(livroJaEstaEmprestado);
-    }
-}
